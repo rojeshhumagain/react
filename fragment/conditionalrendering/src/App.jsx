e1f44774896e1995fdbdc08c32cbc19e09e96290
rojesh <rojeshhumagain@gmail.com>
2026-01-05 15:17:54 +0545

    conditional rendering is completed

---
+++ fragment/conditionalrendering/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+const App = () => {
+  const foodItems = ['apple','banana','berry'];
+
+ 
+  return (
+    <React.Fragment>
+      <h1>Healthy Food</h1>
+      {foodItems.length==0 ?<h3>I am still hungry</h3> : null}
+      <ul className="list-group">
+        {foodItems.map((items) => (
+          <li key={items} className="list-group-items">{items}</li>
+        ))}
+      </ul>
+    </React.Fragment>
+  )
+}
+
+export default App
